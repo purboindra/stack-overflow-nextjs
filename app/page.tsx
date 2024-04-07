152964f6d09a15c57e8e05d43fa0e4c91efd9f0f
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-04-07 13:09:14 +0700

    auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next JS hello</h1>
-      <h2 className="h2-bold">Next JS hello</h2>
-      <h3 className="h3-bold">Next JS hello</h3>
-    </div>
-  );
-}
