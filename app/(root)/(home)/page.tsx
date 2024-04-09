958bbe5b7d17bf34730a705f69155d52a66b9060
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-04-09 10:53:01 +0700

    homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,83 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/contants/filter";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "What is the best programming language for web development?",
+    tags: [
+      {
+        _id: "1",
+        name: "python",
+      },
+      {
+        _id: "2",
+        name: "javascript",
+      },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "",
+    },
+    upvote: 1000000,
+    views: 100,
+    answer: [],
+    creatdAt: new Date("2023-01-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "What is the best programming language for mobile development?",
+    tags: [
+      {
+        _id: "1",
+        name: "flutter",
+      },
+      {
+        _id: "2",
+        name: "javascript",
+      },
+    ],
+    author: {
+      _id: "2",
+      name: "John Doe",
+      picture: "",
+    },
+    upvote: 1532300,
+    views: 90,
+    answer: [],
+    creatdAt: new Date("2022-03-01T12:00:00.000Z"),
+  },
+  {
+    _id: "3",
+    title: "What is the best programming language for backend development?",
+    tags: [
+      {
+        _id: "1",
+        name: "go",
+      },
+      {
+        _id: "2",
+        name: "php",
+      },
+    ],
+    author: {
+      _id: "3",
+      name: "John Doe",
+      picture: "",
+    },
+    upvote: 13,
+    views: 85,
+    answer: [],
+    creatdAt: new Date("2024-01-01T12:00:00.000Z"),
+  },
+];
 
 export default function Home() {
   return (
@@ -31,6 +105,32 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id.toString()}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvote={question.upvote}
+              views={question.views}
+              answers={question.answer}
+              createdAt={question.creatdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no question to show"
+            description="Be the first to break the silence! Ask a Question and kickstart the
+          discussion. Our query could be the next big thing others learn from. Get
+          involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDividedNumber, getTimestamp } from "@/lib/utils";
+
+interface Props {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvote: number;
+  views: number;
+  answers: Array<Object>;
+  createdAt: Date;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvote,
+  views,
+  answers,
+  createdAt,
+}: Props) => {
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
+          <Link href={`/questions/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <RenderTag
+            key={tag._id}
+            _id={Number.parseInt(tag._id)}
+            name={tag.name}
+          />
+        ))}
+        <div className="flex-between mt-6 w-full flex-wrap gap-3 ">
+          <Metric
+            imgUrl="/assets/icons/avatar.svg"
+            alt="user"
+            value={author.name}
+            title={` - ${getTimestamp(createdAt)} `}
+            textStyles="body-medium text-dark400_light700"
+            href={`/profile/${author._id}`}
+          />
+          <Metric
+            imgUrl="/assets/icons/like.svg"
+            alt="upvotes"
+            value={formatAndDividedNumber(upvote)}
+            title="Votes"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            alt="message"
+            value={formatAndDividedNumber(answers.length)}
+            title="Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            alt="eye"
+            value={formatAndDividedNumber(views)}
+            title="Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
