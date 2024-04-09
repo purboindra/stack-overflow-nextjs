6daf4f1364159e91de6968d324514eb1f6a2d952
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-04-09 07:55:22 +0700

    home filters

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -56,8 +56,6 @@ export default function RightSidebar() {
     },
   ];
 
-  popularTags.sort((a, b) => b.totalQuestions - a.totalQuestions);
-
   return (
     <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden">
       <div>
