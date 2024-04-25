10fa9a045ebcff850e430906cf526b35bf5ca268
purboyndra <purboindra@gmail.com>
2024-04-26 06:09:40 +0700

    fix hydration

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 import { Badge } from "../ui/badge";
 import RenderTag from "../shared/RenderTag";
+import UserCardContent from "../shared/UserCardContent";
 
 interface Props {
   user: {
@@ -23,34 +24,7 @@ const UserCard = async ({ user }: Props) => {
       href={`/profile/${user.clerkId}`}
       className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
     >
-      <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
-        <Image
-          src={user.picture}
-          alt="user profile picture"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
-        <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
-            {user.name}
-          </h3>
-          <p className="body-regular text-dark500_light500 mt-2">
-            {user.username}
-          </p>
-        </div>
-        <div className="mt-5">
-          {interactedTags.length > 0 ? (
-            <div className="flex items-center gap-2">
-              {interactedTags.map((tag) => (
-                <RenderTag key={tag._id} name={tag.name} _id={tag._id} />
-              ))}
-            </div>
-          ) : (
-            <Badge>No tags yet</Badge>
-          )}
-        </div>
-      </article>
+      <UserCardContent user={user} interactedTags={interactedTags} />
     </Link>
   );
 };
